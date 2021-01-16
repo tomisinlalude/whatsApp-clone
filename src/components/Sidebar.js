989ad905801fa1d7fe849438a102a5829a4c88b6
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-16 17:39:00 +0100

    Firebase database integration

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
 import SidebarChat from "./SidebarChat";
+import db from '../firebase';
 
 import "./Sidebar.css";
 
 function Sidebar() {
+
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    db.collection('rooms').onSnapshot(snapshot) => 
+      setRooms(
+        snapshot.docs.map((doc) => ({
+        id: doc.id,
+        data: doc.data(),
+      }))
+      )
+  }, [])
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
