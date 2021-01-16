c11bd8b7a2ce9afb89bcb0b1f1197ea981d633c6
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-16 21:58:20 +0100

    Google Auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     //BEM naming convention
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">{/* <h1>Home Screen</h1> */}</Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+        <h1>LOGIN</h1>
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">{/* <h1>Home Screen</h1> */}</Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,40 +5,41 @@ import MoreVert from "@material-ui/icons/MoreVert";
 import React, { useState, useEffect } from "react";
 import "./Chat.css";
 import { InsertEmoticon, Mic } from "@material-ui/icons";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import db from "../firebase";
 
 function Chat() {
   const [seed, setSeed] = useState("");
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const { roomId } = useParams();
-  const [roomName, setRoomName] = useState('');
+  const [roomName, setRoomName] = useState("");
 
   useEffect(() => {
-      if (roomId) {
-        db.collection('rooms').doc(roomId).onSnapshot((snapshot) => {
-            setRoomName(snapshot.data().name);
-        })
-      }
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => {
+          setRoomName(snapshot.data().name);
+        });
+    }
   }, [roomId]);
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
-  }, []);
+  }, [roomId]);
 
   const sendMessage = (e) => {
-      e.preventDefault();
-      console.log("You typed >>>", input);
-      setInput('');
+    e.preventDefault();
+    console.log("You typed >>>", input);
+    setInput("");
   };
-
 
   return (
     <div className="chat">
       <div className="chat__header">
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
-          <h3>Room name</h3>
+          <h3>{roomName}</h3>
           <p>Last seen at...</p>
         </div>
         <div className="chat__headerRight">
