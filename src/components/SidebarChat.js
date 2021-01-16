0a2b3e896c4251cef31697658888fc4ee83feb27
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-16 13:08:47 +0100

    Style sidebar chat

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -8,11 +8,11 @@ function SidebarChat() {
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
     }, [])
-    
+
     return (
         <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-            <div class="sidebarChat__info">
+            <div className="sidebarChat__info">
                 <h2>Room name</h2>
                 <p>Last message...</p>
             </div>
