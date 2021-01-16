115371a5022dd5f54b44179eee69e544496acdd1
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-16 19:56:36 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-
-import './App.css';
-import Sidebar from './components/Sidebar';
-import Chat from './components/Chat';
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
+import { BrowserRouter as Router, Switch, Route } from 'react-dom';
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
-
       <div className="app__body">
-        <Sidebar />
-        <Chat />
+        <Router>
+          <Switch>
+            <Route path="/app">
+              <Sidebar />
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Home Screen</h1>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
