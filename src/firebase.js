8378f83535ba04f6706d9ff4c64080e472814be5
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-16 16:46:06 +0100

    Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -16,3 +16,6 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider};
+export default db;
