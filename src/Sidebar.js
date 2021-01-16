a438539ed750f55823645d07a722c60e0a6cbb42
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-16 10:21:24 +0100

    Sidebar search

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,7 @@ import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 import "./Sidebar.css";
+import { SearchOutlined } from "@material-ui/icons";
 
 function Sidebar() {
   return (
@@ -23,7 +24,10 @@ function Sidebar() {
           </IconButton>
         </div>
       </div>
-      <div className="sidebar__search"></div>
+      <div className="sidebar__search">
+          <SearchOutlined />
+          <input placeholder="Search or start a new chat" type="text" />
+      </div>
       <div className="sidebar__chat"></div>
     </div>
   );
