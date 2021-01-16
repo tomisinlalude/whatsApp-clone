627899a6ab851581ffa249c20b450f8f75709c55
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-16 10:11:27 +0100

    Wrap icons with icon buttons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,30 +1,32 @@
-import React from 'react'
-import { Avatar } from '@material-ui/core';
-import DonutLargeIcon from '@material-ui/icons/DonutLarge';
-import ChatIcon from '@material-ui/icons/Chat';
+import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
-import './Sidebar.css';
+import "./Sidebar.css";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <div className="sidebar__header">
-                <Avatar />
-                <div className="sidebar__headerRight">
-                    <DonutLargeIcon />
-                    <ChatIcon />
-                    <MoreVertIcon />
-                </div>
-            </div>
-            <div className="sidebar__search">
-                
-            </div>
-            <div className="sidebar__chat">
-                
-            </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
-    )
+      </div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chat"></div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
