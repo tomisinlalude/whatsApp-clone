bc56905f6b18cd88ad7b49480f2f9914df11f684
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-16 01:33:01 +0100

    React and Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDtx-o2KpY3lTF3BfgDtKmBg3EsmH45hqM",
+    authDomain: "whatsapp-clone-62cc2.firebaseapp.com",
+    projectId: "whatsapp-clone-62cc2",
+    storageBucket: "whatsapp-clone-62cc2.appspot.com",
+    messagingSenderId: "508055745595",
+    appId: "1:508055745595:web:eedfae491de29d64af181e",
+    measurementId: "G-12K0ZJ07V9"
+  };
