34d03042ff2d83561a09469d8c17b6c0739d33d9
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-16 14:19:05 +0100

    Chat component

---
+++ src/components/Chat.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Chat.css'
+
+function Chat() {
+    return (
+        <div className="chat">
+            <div class="chat__header">
+
+            </div>
+            <div class="chat__body">
+
+            </div>
+            <div class="chat__footer">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Chat
