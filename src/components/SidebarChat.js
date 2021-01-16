3fcd06005d591881dcc87ad0b40660064ff28124
Oluwatomisin <oluwatomisin1605@gmail.com>
2021-01-16 13:17:51 +0100

    Pass props to sidebar chat

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -2,20 +2,28 @@ import { Avatar } from '@material-ui/core';
 import React, { useState, useEffect } from 'react'
 import './SidebarChat.css';
 
-function SidebarChat() {
+function SidebarChat( { addNewChat }) {
     const [seed, setSeed] = useState(''); 
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
     }, [])
 
-    return (
+    const createChat = () = {
+
+    };
+
+    return !addNewChat ? (
         <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             <div className="sidebarChat__info">
                 <h2>Room name</h2>
                 <p>Last message...</p>
             </div>
+        </div>
+    ) : (
+        <div onClick={createChat} className="sidebarChat">
+            <h2>Add new chat</h2>
         </div>
     )
 }
